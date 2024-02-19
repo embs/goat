2fe748a581edff6e07482ca35b7679647ee0b7bf
Matheus Santana <edmatheus.santana@gmail.com>
2024-02-19 18:34:58 -0300

    use aws-sdk-rails to enqueue active job messages to SQS

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsLambdaWorker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :amazon
   end
 end
